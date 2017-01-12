b3a7b36cefe04686d639e2f5a7e6560ee42007ca
佐藤浩仁 <hirodon@hirodonMacBook-Pro.local>
2017-01-12 17:44:31 +0900

    2017/1/12/17:22

--- js/voice_chat.js
+++ js/voice_chat.js
@@ -2,5 +2,6 @@ $(function () {
 	var peer = new Peer({key: 'e6ff67a4-887b-47fd-9cab-841b8f0346e6'});
 	peer.on('open', function(id) {
 	  	console.log('My peer ID is: ' + id);
+	  	$('test_btn').text(id);
 	});
 });
